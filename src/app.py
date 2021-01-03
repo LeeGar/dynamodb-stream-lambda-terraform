4be3dc2fd5bcc3b647a84103b3bba4ec68c44a8f
Gar Lee <gardarr@gmail.com>
2021-01-03 15:10:10 -0800

    Remove count

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 
 def handler(event, context):
-    count = 0
     for record in event['Records']:
         hash_key = record['dynamodb']['Keys']['TestTableHashKey']['S']
+        print("Received message from DynamoDB Stream!")
         print(f"Dynamo hash key: {hash_key}")
+        
